64053963894b83ce6da8f6422cb3c78e05c412a2
ARCADIA\anton.drozdov <anton.drozdov@arcadia.spb.ru>
2019-01-04 13:50:35 +0300

    142018107 fix develop branch

--- kcconstruction/gulpfile.js
+++ kcconstruction/gulpfile.js
@@ -1,8 +1,7 @@
-let gulp = require('gulp');
-let sass = require('gulp-sass');
-
-let rimraf = require('rimraf');
-
+let gulp = require("gulp");
+let fs = require("fs");
+let sass = require("gulp-sass");
+let rimraf = require("rimraf");
 let paths = {
     nodeModuleRoot: "./node_modules/",
     webRoot: "./wwwroot/"
@@ -10,16 +9,16 @@ let paths = {
 
 
 gulp.task("processingSass", function () {
-    return gulp.src('Styles/StyleSheet.scss')
+    return gulp.src("Styles/StyleSheet.scss")
     .pipe(sass())
-        .pipe(gulp.dest('wwwroot/css'));
+        .pipe(gulp.dest("wwwroot/css"));
 });
 
 gulp.task('copyingFontAwesome', function() {
-    return gulp.src(paths.nodeModuleRoot + 'font-awesome/**/*')
-        .pipe(gulp.dest(paths.webRoot + 'libs/font-awesome/'));
+    return gulp.src(paths.nodeModuleRoot + "font-awesome/**/*")
+        .pipe(gulp.dest(paths.webRoot + "libs/font-awesome/"));
 });
 
 gulp.task('cleanFontAwesome', function(callback) {
-    rimraf(paths.webRoot + 'libs/font-awesome', callback);
+    rimraf(paths.webRoot + "libs/font-awesome", callback);
 });

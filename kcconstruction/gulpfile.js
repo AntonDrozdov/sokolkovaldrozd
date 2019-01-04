5854cc065fae3fb03e18bbb330a0ae6452cb7a38
ARCADIA\anton.drozdov <anton.drozdov@arcadia.spb.ru>
2019-01-04 18:26:50 +0300

    142018107 NW try to construct gallery

--- kcconstruction/gulpfile.js
+++ kcconstruction/gulpfile.js
@@ -7,22 +7,16 @@ let paths = {
     webRoot: "./wwwroot/"
 };
 
-
 gulp.task("processingKCSass", function () {
     return gulp.src("Styles/KCStyleSheet.scss")
     .pipe(sass())
         .pipe(gulp.dest("wwwroot/css"));
 });
+
 gulp.task("processingKCJs", function () {
     return gulp.src("Scripts/KCScripts.js")
         .pipe(gulp.dest("wwwroot/js"));
 });
 
-gulp.task('copyingFontAwesome', function() {
-    return gulp.src(paths.nodeModuleRoot + "font-awesome/**/*")
-        .pipe(gulp.dest(paths.webRoot + "libs/font-awesome/"));
-});
 
-gulp.task('cleanFontAwesome', function(callback) {
-    rimraf(paths.webRoot + "libs/font-awesome", callback);
-});
+
--- kcconstruction/wwwroot/js/KCScripts.js
+++ kcconstruction/wwwroot/js/KCScripts.js
@@ -2,7 +2,8 @@ $(document).ready(function () {
 
     openImage = function (pathToImageSm) {
         var pathToImage = pathToImageSm.replace('_sm.jpg', '.jpg');
-        var newImage = "<img src=\"" + pathToImage + "\" class=\"kc-main-gallery-image\"/>";
+        //var newImage = `<i class="fas fa-chevron-left"></i><img src="${pathToImage}" class="kc-main-gallery-image"/><i class="fas fa-chevron-right"></i>`;
+        var newImage = `</i><img src="${pathToImage}" class="kc-main-gallery-image"/>`;
 
         $("#pictureDemonstratorContainer").html(newImage);
 

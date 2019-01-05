56e7be580a3223790afc1e591d52dc56e3f17f6d
ARCADIA\anton.drozdov <anton.drozdov@arcadia.spb.ru>
2019-01-05 16:30:27 +0300

    152019154 NW try to construct gallery

--- kcconstruction/wwwroot/js/KCScripts.js
+++ kcconstruction/wwwroot/js/KCScripts.js
@@ -1,33 +1,68 @@
 $(document).ready(function () {
 
-    showGallery = function (idOfSourceInput, pathToCurrentImageSm) {
-        debugger;
+    
+     showGallery = function (idOfSourceInput, pathToCurrentImageSm) {
         $("#pictureDemonstratorSourceInput").val( $("#" + idOfSourceInput).val());
 
-        showCurrentImage(pathToCurrentImageSm);
+         showImage(pathToCurrentImageSm);
+
+         $("#pictureDemonstrator").modal('show');
     };
 
-    showCurrentImage = function(pathToCurrentImage) {
-        var pathToImage = pathToCurrentImage.replace('_sm.jpg', '.jpg');
-
-        var newImage = `</i><img src="${pathToImage}" class="kc-main-gallery-image"/>`;
-
-        $("#pictureDemonstratorContainer").html(newImage);
-
-        $("#pictureDemonstrator").modal('show');
-    }
-
-    showNextImage = function (pathToCurrentImageSm) {
-
+    showImage = function (pathToCurrentImageSm) {
+          
         var pathToImage = pathToCurrentImageSm.replace('_sm.jpg', '.jpg');
 
-        var newImage = `</i><img src="${pathToImage}" class="kc-main-gallery-image"/>`;
+        $("#pictureDemonstratorCurrentImage").val(pathToImage);
+
+        var newImage = `<table><tr><td><img src="/images/10pxtemplate.jpg"/></td>
+                                    <td><img src="${pathToImage}" class="kc-main-gallery-image"/></td>
+                                </tr></table>`;
+
+
 
         $("#pictureDemonstratorContainer").html(newImage);
 
-        $("#pictureDemonstrator").modal('show');
 
+        //$("#pictureDemonstratorContainer").animate({ opacity: 0 }, 1000,
+        //    function() {
+        //        $(this).html(newImage).find("table>tr>td>img").bind("load",
+        //            function() {
+        //                $(this).parent().animate({ opacity: 1 }, 1000);
+        //            });
+        //    });
     }
 
+    showNextImage = function (direction) {
+        let currentImage = $("#pictureDemonstratorCurrentImage").val();
+        let imageList = $("#pictureDemonstratorSourceInput").val().split(';');
+
+        let indexOfNextImage = 0;
+
+        let indexOfCurrentImage = imageList.lastIndexOf(currentImage);
+
+        if (direction === "forward") {
+            if (indexOfCurrentImage !== imageList.length - 1) {
+                indexOfNextImage = indexOfCurrentImage + 1;
+            }
+        }
+        if (direction === "back") {
+            if (indexOfCurrentImage !== 0) {
+                indexOfNextImage = indexOfCurrentImage - 1;
+            } else {
+                indexOfNextImage = imageList.length - 1;
+            }
+        }
+
+        showImage(imageList[indexOfNextImage]);
+    }
+
+    $('#pictureDemonstrator-nextImage').on("click", function (evnt) {
+        showNextImage("forward");
+    });
+
+    $('#pictureDemonstrator-previousImage').on("click", function (evnt) {
+        showNextImage("back");
+    });
 
 });

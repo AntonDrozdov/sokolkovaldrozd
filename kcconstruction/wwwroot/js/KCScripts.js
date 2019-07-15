860d9d48e550673389b999ccc64f4f1bc381b27a
ARCADIA\anton.drozdov <anton.drozdov@arcadia.spb.ru>
2019-07-15 12:54:16 +0300

    add jquery validation + 1 partner - arendabeton.ru

--- kcconstruction/wwwroot/js/KCScripts.js
+++ kcconstruction/wwwroot/js/KCScripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function (event) {
     
     showGallery = function (idOfSourceInput, pathToCurrentImageSm) {
         $("#pictureDemonstratorSourceInput").val( $("#" + idOfSourceInput).val());
@@ -72,14 +72,22 @@ $(document).ready(function () {
         showFeedbackForm();
     });
 
-    showFeedbackForm = function () {
+    showFeedbackForm = function() {
         $("#formfeedbackInputs-successPost").hide();
         $("#formfeedbackInputs-failPost").hide();
         $("#formfeedbackInputs").show();
         $('#feedbackForm').modal({ backdrop: 'static', keyboard: false });
-    }
+    };
+
+    $("#kc-feedbackForm-validatePart").submit(function(event) {
+         event.preventDefault();
+    });
 
     sendFeedbackForm = function () {
+
+        if (!validateFeedbackForm()) {
+            return;
+        }
 
         var formData = new FormData($("form").get(0));
 
@@ -107,15 +115,66 @@ $(document).ready(function () {
 
     };
 
-
-    showPostFeedbackFormResult = function (elementName) {
+    showPostFeedbackFormResult = function(elementName) {
         $("#formfeedbackInputs").hide();
 
         $("#" + elementName).show();
 
         setTimeout(function() {
-            $("#" + elementName).hide();
-            $("#feedbackCloseBtn").click();
-        }, 5000);
-    }
+                $("#" + elementName).hide();
+                $("#feedbackCloseBtn").click();
+            },
+            5000);
+    };
+
+    validateFeedbackForm = function() {
+        $("#kc-feedbackForm-validatePart").validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                phone: {
+                    required: true,
+                    digits: true,
+                    minlength: 3,
+                    maxlength: 20 
+                },
+                message: {
+                    required: true,
+                    minlength: 10,
+                    maxlength: 3000 
+                }
+
+            },
+            messages: {
+                name: {
+                    required: "*Как мы можем к Вам обращаться",
+                    minlength: "*Не менее 2х символов"
+                },
+                email: {
+                    required: "*Введите адрес электронной почты",
+                    email:"*Неккоректный адрес"
+                },
+                phone: {
+                    required: "*Введите номер телефона",
+                    digits: "*Допустимы только цифры",
+                    minlength: "*Не менее 3х символов",
+                    maxlength: "*Не более 20ти символов"
+                },
+                message: {
+                    required: "*Поле не может быть пустым",
+                    minlength: "*Не менее 10ти символов",
+                    maxlength: "*Не более 3000 символов"
+                }
+
+            }
+        });
+
+        return $("#kc-feedbackForm-validatePart").valid();
+    };
 });
